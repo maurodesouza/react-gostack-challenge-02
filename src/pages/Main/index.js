6452c589f36d569ace71630c9cd34a768d057bba
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-16 00:35:52 -0300

    class 10 - routes navigation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Container, Form, SubmitButton, List } from './styles';
@@ -85,7 +86,9 @@ export default class Main extends Component {
           {repositories.map(({ name }) => (
             <li key={name}>
               <span> {name} </span>
-              <a href="##"> Detalhes </a>
+              <Link to={`/repository/${encodeURIComponent(name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
