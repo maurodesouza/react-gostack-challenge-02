55099036de0a7c50fec41889be635a13c329ed23
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-16 01:10:24 -0300

    class 12 - defining proptypes

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import api from '../../services/api';
 
@@ -42,3 +43,11 @@ export default class Repository extends Component {
     return <h1> Repository</h1>;
   }
 }
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repository: PropTypes.string,
+    }),
+  }).isRequired,
+};
